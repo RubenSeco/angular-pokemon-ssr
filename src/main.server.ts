29b53ee5fc06342a54badcc76f4001ec0be3e65a
Rubén Seco <ruben.seco.rs@gmail.com>
2025-08-04 08:55:31 -0300

    update 4-8-25

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { Config } from "@netlify/edge-functions"
 
+export const edgeConfig: Config = {
+  path: "/hello",
+  onError: "bypass"
+}
 const bootstrap = () => bootstrapApplication(AppComponent, config);
+
 
 export default bootstrap;
